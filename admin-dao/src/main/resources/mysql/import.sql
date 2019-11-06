fe90594aa529e4f3981ac61322a0faa0bed50e0c
fanxl12 <744822514@qq.com>
2019-11-06 21:10:47 +0800

    添加数据导入;

---
+++ admin-dao/src/main/resources/mysql/import.sql
@@ -0,0 +1,2 @@
+insert into `admin_user` (`id`, `username`, `password`, `salt`, `phone`, `head`, `email`)
+values ('TUCljI4LcJCCks6t5QQcp74QrS03Wcz4','admin','$2a$10$qonwEOUctDkDZxoFRYDSoOVFsxfLrqz7KqwZWut3yrFLKtya/s2Jq', NULL, '17621190030', NULL,'1964645988@qq.com');
